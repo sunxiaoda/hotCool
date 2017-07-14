943ba443290a3e8add4b946046f07e68b09cdc33
孙孝达imac <874058523@qq.cin>
2017-07-14 15:39:02 +0800

    test push

---
+++ HotcoolDemo/Classes/Base/SunBaseViewController.m
@@ -0,0 +1,39 @@
+//
+//  SunBaseViewController.m
+//  HotcoolDemo
+//
+//  Created by zero on 2017/6/18.
+//  Copyright © 2017年 cl. All rights reserved.
+//
+
+#import "SunBaseViewController.h"
+
+@interface SunBaseViewController ()
+
+@end
+
+@implementation SunBaseViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"test");
+
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
